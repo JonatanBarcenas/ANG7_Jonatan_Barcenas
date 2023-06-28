b01dfb439094315e5a61801c8c237e286b95ae39
JonatanBarcenas <L21650344@zitacuaro.tecnm.mx>
2023-06-27 21:52:21 -0600

    Tutorial 9

--- GymManagerFront/src/app/pages/sign-in/sign-in.module.ts
+++ GymManagerFront/src/app/pages/sign-in/sign-in.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SignInRoutingModule } from './sign-in-routing.module';
 import { SignInComponent } from './sign-in.component';
+import { LoginFormModule } from 'src/app/components/login-form/login-form.module';
+
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { SignInComponent } from './sign-in.component';
   ],
   imports: [
     CommonModule,
-    SignInRoutingModule
+    SignInRoutingModule,
+    LoginFormModule
   ]
 })
 export class SignInModule { }
